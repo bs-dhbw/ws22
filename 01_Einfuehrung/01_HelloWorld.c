be28d7a98da49563115643bdda4f40f73f55b694
bdk <mail@d-bauer.net>
2022-06-29 18:37:28 +0200

    add HelloWorld

---
+++ 01_Einfuehrung/01_HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+ 
+/*  
+ * Compile via:  
+ * gcc -Wall 01_HelloWorld.c -o HelloWorld 
+ * Run with:  
+ * ./HelloWorld 
+ */ 
+ 
+int main (void) 
+{ 
+  printf("Hello World\r\n"); 
+}
