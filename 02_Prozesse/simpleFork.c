319213c3f3ed6629d98142a8d01606a42ec0f763
vl <bs.dhbw@gmail.com>
2022-09-26 21:45:52 +0200

    Add fork bomb example

--- 02_Prozesse/simpleFork.c
+++ 02_Prozesse/simpleFork.c
@@ -2,31 +2,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(void)
-{
-    printf("Hello World...\n");
+int main(void) {
+  printf("Hello World...\n");
 
-    pid_t pid = fork();
+  pid_t pid = fork();
 
-    if (pid == 0)
-    {
-      /* Child process */
-      printf("Inside child - PID: %d\n", getpid());
-    }
-    else if (pid > 0)
-    {
-      /* Parent process */
-      printf("Inside parent - PID of child process: %d\n", pid);
-      printf("Inside parent - PID: %d\n", getpid());        
-    }
-    else
-    {
-      /* Error durring fork() */
-      printf("fork() failed!\n");
-      return EXIT_FAILURE;
-    }
+  if (pid == 0) {
+    /* Child process */
+    printf("Inside child - PID: %d\n", getpid());
+  } else if (pid > 0) {
+    /* Parent process */
+    printf("Inside parent - PID of child process: %d\n", pid);
+    printf("Inside parent - PID: %d\n", getpid());
+  } else {
+    /* Error durring fork() */
+    printf("fork() failed!\n");
+    return EXIT_FAILURE;
+  }
 
-    printf("Bye World...\n");
+  printf("Bye World...\n");
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
