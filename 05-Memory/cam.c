a3af58ca29acd56b3f4bc8224788892aaeaaec27
vl <bs.dhbw@gmail.com>
2022-10-18 18:55:35 +0200

    Update CAM

--- 05-Memory/cam.c
+++ 05-Memory/cam.c
@@ -1,6 +1,3 @@
-/* Vorlagedatei zu E1 Aufgabe 6 (Speicherverwaltung) */
-/* 17.06.05/gle */
-
 #include <stdio.h>
 
 int main() {
@@ -8,9 +5,7 @@ int main() {
                           0x9269, 0xc645, 0xaffe, 0x0000, 0x7e3b, 0x038a};
   _Bool valid[12] = {0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1};
 
-  unsigned short cam2[10] = {0xd2, 0xf1, 0xed, 0x53, 0xa2,
-                             0x79, 0x14, 0xde, 0x03, 0xd5};
-  _Bool valid2[10] = {1, 1, 0, 1, 1, 1, 1, 0, 1, 1};
+
   unsigned int muster, maske;
 
   printf("Bitte Musterwert eingeben: ");
@@ -21,14 +16,7 @@ int main() {
 
   printf("muster: %x   maske %x   \n", muster, maske);
 
-  for (unsigned int i = 0; i < 10; i++) {
-    if (valid2[i] == 1) {
-      if ((muster & maske) == (cam2[i] & maske)) {
-        printf("Hit bei %d", i);
-        printf("cam: %x ", cam2[i]);
-      }
-    }
-  }
+/* TODO */ 
 
   return 0;
 }
